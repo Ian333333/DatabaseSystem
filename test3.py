0a3cc746659d6549011f7b1f07fd182b0892281d
ian <yimin.2020.chen@outlook.com>
2022-12-27 18:40:44 -0800

    Change variable names

--- test3.py
+++ test3.py
@@ -14,15 +14,15 @@ e.insert(table_name='test_t1', f_name='test_06', f_age=60)
 
 print("-" * 5, "This is all the data in test_t1", "-" * 5)
 all_data = e.search("test_t1")
-for index in all_data:
-    print(index)
+for data in all_data:
+    print(data)
 print("-" * 30)
 
 
 print("-" * 5, "Search for all users over 30", "-" * 5)
 test_data_01 = e.search(table_name="test_t1", f_age=GreaterCase(30))
-for index in test_data_01:
-    print(index)
+for data in test_data_01:
+    print(data)
 print("-" * 30)
 
 
